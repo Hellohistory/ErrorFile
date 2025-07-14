5b68435699d43c793dcefa9f2901cd2a86fd10fa
Hellohistory <etojsyc521@gmail.com>
2025-07-14 21:13:48 +0800

    [update] github-ci-cd

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ErrorFile.Detection.FileInspector import FileInspector
 
 
-file_path = r'D:\Code\Python Work\ErrorFile\TestFiles\TOM_损坏.jpg'
-image_mode = 'precise'  # 或 'fast'
+file_path = r'tests/files/TOM_损坏.jpg'
+image_mode = 'fast'  # 或 'fast'
 result = FileInspector(file_path, image_mode).inspect()
 print(result)
